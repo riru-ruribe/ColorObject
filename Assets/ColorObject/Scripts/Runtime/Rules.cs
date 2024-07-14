9a55c8e96f3fd34bd035bf1a6044acadfe48cdda
riru-ruribe <riru.ruribe@gmail.com>
2024-07-14 09:20:02 +0900

    RefMata用の変換ルール

---
+++ Assets/ColorObject/Scripts/Runtime/Rules.cs
@@ -0,0 +1,9 @@
+﻿namespace ColorObject
+{
+    public static class Rules
+    {
+        public const string Select = "Select<UnityEngine.Object, Color>(x => (ColorObject.ColorObject)x)";
+        public const string _Select = "." + Select;
+        public const string Select_ = Select + ".";
+    }
+}
