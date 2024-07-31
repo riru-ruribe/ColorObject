d66d5aa1845370c5f596f39078038d96718a5cd9
riru-ruribe <riru.ruribe@gmail.com>
2024-07-31 21:18:52 +0900

    フォルダ単位で定数にする

---
+++ Assets/ColorObject/Samples~/Sample1/ColorKey.cs
@@ -0,0 +1,7 @@
+﻿namespace ColorObject.Samples
+{
+    [ColorKey("Assets/SampleLink/Sample1")]
+    public partial class ColorKey
+    {
+    }
+}
---
+++ ColorKeyGenerator/DiagnosticDescriptors.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace ColorKeyGenerator;
+
+public static class DiagnosticDescriptors
+{
+    const string Category = "ColorKey";
+
+    public static readonly DiagnosticDescriptor E0001 = new(
+        id: Category + nameof(E0001),
+        title: "invalid accessibility",
+        messageFormat: "'public' or 'protected' or 'internal' or 'private' is allowed.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor E0002 = new(
+        id: Category + nameof(E0002),
+        title: "invalid syntax",
+        messageFormat: "'partial' class required.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor E0003 = new(
+        id: Category + nameof(E0003),
+        title: "invalid arg",
+        messageFormat: "directory not exist.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+}
